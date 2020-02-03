83ce0097632c190cd79b78f654770ebd16861c6d
James Ritchie <jritchie@mercuryholidays.com>
2020-02-03 09:40:28 +0000

    all code moved into main.go

--- src/booking/main.go
+++ src/booking/main.go
@@ -23,3 +23,29 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+var _ microservice.Service = (*service)(nil)
+
+type service struct {
+	messagebus context.Context // just using context as an example interface
+	running    chan error
+}
+
+type bus interface {
+}
+
+func (s *service) Start(ctx context.Context) error {
+	s.running = make(chan error, 1)
+	defer close(s.running)
+
+	// start http / grpc servers here
+	// if they are non-blocking, block on context
+	// remember to monitor the context for cancellation
+
+	<-ctx.Done()
+	return nil
+}
+
+func (s *service) Shutdown(ctx context.Context) error {
+	return <-s.running
+}
